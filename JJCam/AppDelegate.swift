3a5df908187d4626b5259aeed301fd0157f964cf
Jairo Pereira Junior <jairojr.p@hotmail.com>
2021-04-23 22:02:37 -0300

    bugfix

--- JJCam/AppDelegate.swift
+++ JJCam/AppDelegate.swift
@@ -30,6 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // Saves changes in the application's managed object context when the application transitions to the background.
+        UIApplication.shared.isIdleTimerDisabled = false
         self.saveContext()
     }
 
